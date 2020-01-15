fec12189442315c794fb4d0ea7a86d112815d7c9
Dylan Van Assche <dylan.van.assche@protonmail.com>
2020-01-15 18:27:22 +0100

    [alsa] EC25 modem is now card 0, unmute script has to unmute the A64 sound card only

--- sparse/usr/bin/droid/unmute-sound-card.sh
+++ sparse/usr/bin/droid/unmute-sound-card.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Unmute the A64 sound card at boot
+CARD=1 # A64 sound card
 
-amixer -c 0 set 'AIF1 Slot 0 Digital DAC' unmute
-amixer -c 0 set 'Line Out' unmute
-amixer -c 0 set 'Line Out' 100%
+amixer -c $CARD set 'AIF1 Slot 0 Digital DAC' unmute
+amixer -c $CARD set 'Line Out' unmute
+amixer -c $CARD set 'Line Out' 100%
