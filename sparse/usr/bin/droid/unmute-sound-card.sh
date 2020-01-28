2faca790be6238905d339e39045e415652cce682
Sergey Chupligin <neochapay@gmail.com>
2020-01-28 18:21:04 +0300

    [config] Add voicecall rules for pulseaudio

--- sparse/usr/bin/droid/unmute-sound-card.sh
+++ sparse/usr/bin/droid/unmute-sound-card.sh
@@ -5,5 +5,12 @@ amixer -c $CARD set 'AIF1 Slot 0 Digital DAC' unmute
 amixer -c $CARD set 'Line Out' unmute
 amixer -c $CARD set 'Line Out' 100%
 
+#Modem intut set to mono
+amixer -c $CARD set 'Line Out Source' 'Mono Differential'
+
 # Set A64 sound card as default sink for Pulseaudio
 pacmd set-default-sink alsa_output.platform-sound.stereo-fallback
+
+#Setup voicecall route
+pactl load-module module-loopback source=alsa_input.platform-sound.stereo-fallback sink=alsa_output.platform-sound-modem.analog-mono
+pactl load-module module-loopback source=alsa_input.platform-sound-modem.analog-mono sink=alsa_output.platform-sound.stereo-fallback
