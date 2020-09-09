2b37032bf046357a5a8fd58ffba571adc3c38433
Adam Pigg <adam@piggz.co.uk>
2020-09-09 07:54:12 +0100

    Port enable/disable modem to the new modem-power driver in kerne 5.9

--- sparse/usr/bin/droid/disable-modem.sh
+++ sparse/usr/bin/droid/disable-modem.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Disable EG25 modem
-# GPIO export is done by enable-modem.sh
-# GPIO35 is PWRKEY
-# GPIO68 is RESET_N
-# GPIO232 is W_DISABLE
-
-# Turn RESET_N on
-echo 1 > /sys/class/gpio/gpio68/value
-
-# Turn W_DISABLE on
-echo 1 > /sys/class/gpio/gpio232/value
-
-# Trigger PWRKEY and wait until the modem has been turned off
-echo 1 > /sys/class/gpio/gpio35/value && sleep 2 && echo 0 > /sys/class/gpio/gpio35/value
-sleep 30
+echo 0 > /sys/class/modem-power/modem-power/device/powered
